b0dac4cdbd8aff22dfc15a6e2defdb2b36e4b63c
SANJEETH KAMIREDDI <53048323+sudha9848@users.noreply.github.com>
2020-11-17 21:36:04 +0530

    Add files via upload

---
+++ 1st question.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n=7;
+	for (int i = 0; i < n; i++) {
+
+			for (int j = 1; j <= i+1; j++)
+            {
+				cout<<" ";
+			}
+
+			for (int j = i+1; j <=n; j++)
+            {
+				cout<<j<<" ";
+			}
+	cout<<"\n";
+}
+	for(int i=n;i>1;i--)
+	{
+		for(int j=i;j>1;j--)
+		{
+			cout<<" ";
+		}
+		for(int j=i-1;j<=n;j++)
+		{
+			cout<<j<<" ";
+		}
+		cout<<"\n";
+	}
+
+return 0;
+}
